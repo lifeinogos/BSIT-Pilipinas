a81fb76f01c4b22e57ecb1bd3f0ffc9faa16d3cb
Katrina Curbi <31692151+inspiritmelody@users.noreply.github.com>
2017-10-17 14:49:19 +0800

    Add files via upload

---
+++ Kat/about.php
@@ -0,0 +1,122 @@
+<!-- About page -->
+
+	 <nav class="navbar navbar-custom navbar-fixed-top text-center">
+					
+						<div class="navbar-header" >
+						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>                        
+						  </button>
+							<img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Los Angeles">
+						
+						</div>
+						
+			<div  class="collapse navbar-collapse" id="myNavbar">
+						
+				<ul class="nav navbar-nav">
+							<li><a href="<?php echo base_url('Pilipinas/home/');?>">Home</a></li>
+							<li><a href="<?php echo base_url('Pilipinas/travel/');?>">Travel</a></li>
+							<li><a href="<?php echo base_url('Pilipinas/history/');?>">History</a></li>
+							<li class="active"><a href="<?php echo base_url('Pilipinas/about/');?>">About</a></li>				
+			    </ul> 
+						  
+				 <ul class = "nav navbar-nav " style = "margin-top: -10px;">
+						  <li> 
+								<form action="" method="get" id="search">	
+									<input type="text" name="search" class="search" value = "Search places"/> 
+									<input type="submit" value="Search" />
+								</form> 
+						  </li>
+				 </ul>		 		 
+			</div>
+	</nav>
+<div class = "container-fluid" style = "background-image: url('img/background')">	
+
+			<div class = "row">
+			<br />
+			<br />
+			 
+				  <div class="item">
+					<img src="<?php echo base_url('img/slider_2.jpg') ?>" alt="" style="width:100%;">
+				  </div>
+				  
+			</div>
+
+	
+
+	
+	<div class = "row text-center" id ="embed">
+	<br />
+		<div class = "col-md-6">
+			<img src="<?php echo base_url('img/main.jpg') ?>" alt="" style="width:100%; padding-bottom:20px;">
+		</div>
+	
+		<div class = "col-md-6">
+			<h1> ABOUT US </h1>
+			<h3>PILIPINAS serves as an online tourist hub showcasing detailed information about the country’s heritage, culture, and natural landmarks.</h3>
+									  
+			<p>This website will also promote local businesses by advertising their products and services for free; like local-made souvenirs and local travel guides.
+								
+				Pili-Pinas is a website that aims to attract Locals and International Tourists who wish to travel around The Philippines.
+				It also aims to advocate local businessmen in promoting Filipino-pride goods and services.</p>
+				
+		</div>
+	</div>
+	
+		<div class = "col-md-6">
+		<br />
+			<img src="<?php echo base_url('img/featured_1.jpg') ?>" alt="" style="width:90%; padding-bottom:20px; padding-left:5px; ">
+		</div>
+
+		<div class = "col-md-6">
+			<h1>PILIPINAS</h1>
+			<p>has the following features:
+					<ul>
+						<li>One-stop interactive detailed website for locating all Philippine’s tourist destinations</li>
+						<li>List and galleries of places categorized accordingly</li>
+						<li>Suggestion based on filters chosen by the tourist</li>
+						<li>Integrated local travel guide offers</li>
+						<li>Voting for popularity and experience</li>
+						<li>Reviews that will help in suggestion for future development of that place</li>
+						<li>Advertisement for local products on it’s respective places</li>
+						<li>Estimated minimum budget needed for visiting the spot</li>
+					</ul>
+			
+			</p>
+		</div>
+	<div class="row">
+	</div>
+
+	
+	
+	<!-- footer -->
+	<div class = "row text-center" id = "footer">
+	
+		<div class = "col-md-6">
+			<h4> DISCOVER MORE </h4>
+			<p style="padding-left: 10px;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+			Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa</p>
+			
+		</div>
+		<div class = "col-md-2">
+			<h4> GET CONNECTED </h4>
+			<a href =""><img src = "<?php echo base_url('img/facebook.png') ?>" width = "15%" alt="Facebook"> Like us on Facebook </a>
+			<a href =""><img src = "<?php echo base_url('img/twitter.png') ?>" width = "18%" alt="Twitter"> Follow us on Twitter </a>
+			
+		</div>
+		<div class = "col-md-2">
+			<h4> CONTACT INFORMATION </h4>
+			<img src = "<?php echo base_url('imgg/address.png') ?>" width = "15%" alt="Address"><h6>PILI-PINAS HATE CORNER MANILA, PHILIPPINES</h6>
+			<img src = "<?php echo base_url('imgg/mobile.png') ?>" width = "15%" alt="Phone"><h6>(+63) 9152987364</h6>
+			<img src = "<?php echo base_url('imgg/email.png') ?>" width = "15%" alt="Mail"><h6>pili_pinas@gmail.com</h6>
+		
+		</div>
+		
+		<div class = "col-md-2">
+		</div>
+		
+	</div>
+</div>
+
+</div>
