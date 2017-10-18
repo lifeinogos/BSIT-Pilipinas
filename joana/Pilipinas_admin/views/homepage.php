fb4515b604f872ac0596d9c9658a5b26fc5c5b85
Joanalyn <31683116+jlynvz@users.noreply.github.com>
2017-10-18 13:49:55 +0800

    Add files via upload

---
+++ joana/Pilipinas_admin/views/homepage.php
@@ -0,0 +1,195 @@
+<!-- Homepage -->
+	  
+	 <nav class="navbar navbar-custom navbar-fixed-top text-center">
+					
+						<div class="navbar-header" >
+						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>                        
+						  </button>
+							<img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Los Angeles">
+						
+						</div>
+						
+			<div  class="collapse navbar-collapse" id="myNavbar">		
+				<ul class="nav navbar-nav">
+							<li class="active" style = "border-radius: 0px 30px 0px 30px; background-color: gray;"><a href="<?php echo base_url('Pilipinas/homepage');?>"> Home </a></li>
+							<li><a href="<?php echo base_url('Pilipinas/places/');?>"> Places </a></li>
+							<li><a href="<?php echo base_url('Pilipinas/advertisement/');?>"> Advertisement </a></li>
+							<li><a href="<?php echo base_url('Pilipinas/settings/');?>"> Settings	</a></li>			
+			    </ul> 		  		 
+			</div>
+	</nav>
+	
+<div class = "container-fluid" style = "background-image: url('img/background');">	
+	
+<div class = "row ">
+	<br />
+	<br />
+	<br />			 
+				  <div class="item">
+					<img src="<?php echo base_url('img/slider_11.jpg') ?>" alt="" style="width:100%;">
+				  </div>
+				  
+</div>
+		<div class ="col-md-12">
+		<h1 style="color:#C92E2E; text-align: center;"> WELCOME TO PILIPINAS DASHBOARD! </h1>
+		</div>
+</div>
+
+<br />
+
+<div class="col-md-4">	 
+        <div class="thumbnail">
+		<h3 style="color:#C92E2E; text-align: center;"> NUMBER OF VIEWS</h3>
+					<!-- TABLE OF RESERVATION -->
+					<table id="customers_B">
+						<thead>
+							<th> NO.</th>
+							<th> IDENTIFICATION NO</th>
+							<th> CLIENT NAME</th>
+							<th> ACTION</th>
+							
+						</thead>
+						<tr>
+							<td> 10 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View  </button> </td>
+						</tr>
+						<tr>
+							<td> 9 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						<tr>
+							<td> 8 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View  </button> </td>
+						</tr>
+						<tr>
+							<td> 7 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						<tr>
+							<td> 6 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						
+						 
+					</table>
+		</div>		
+					
+</div>
+
+<div class="col-md-4">	
+               <div class="thumbnail">
+		<h3 style="color:#C92E2E;; text-align: center;"> LIST OF PRODUCTS </h3>
+					<!-- TABLE OF RESERVATION -->
+					<table id="customers_B">
+						<thead>
+							<th> NO.</th>
+							<th> PRODUCT CODE</th>
+							<th>PRODUCT NAME</th>
+							<th> ACTION</th>
+							
+						</thead>
+						<tr>
+							<td> 10 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						<tr>
+							<td> 9 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						<tr>
+							<td> 8 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						<tr>
+							<td> 7 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						<tr>
+							<td> 6 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						
+						 
+					</table>
+		</div>		
+</div>
+
+<div class="col-md-4">	
+              <div class="thumbnail">
+		<h3 style="color:#C92E2E; text-align: center;"> LIST OF PLACES</h3>
+					<!-- TABLE OF RESERVATION -->
+					<table id="customers_B">
+						<thead>
+							<th> NO.</th>
+							<th> IDENTIFICATION NO</th>
+							<th> CLIENT NAME</th>
+							<th> ACTION</th>
+							
+						</thead>
+						<tr>
+							<td> 10 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View </button> </td>
+						</tr>
+						<tr>
+							<td> 9 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View  </button> </td>
+						</tr>
+						<tr>
+							<td> 8 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View  </button> </td>
+						</tr>
+						<tr>
+							<td> 7 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View  </button> </td>
+						</tr>
+						<tr>
+							<td> 6 </td>
+							<td> MM-DD-YY-INC</td>
+							<td> Christina Berglund</td>
+							<td> <button type="button" class="btn btn-primary"> View  </button> </td>
+						</tr>
+						
+						 
+					</table>
+		</div>		
+</div>
+
+	
+	
+
+
+
+
+	
+</div> <!-- end of container-fluid -->
