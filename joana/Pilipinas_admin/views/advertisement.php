514e1b5f61c1201ed46940db062937bcc5eb7737
Joanalyn <31683116+jlynvz@users.noreply.github.com>
2017-10-18 14:00:14 +0800

    Add files via upload

---
+++ joana/Pilipinas_admin/views/advertisement.php
@@ -0,0 +1,37 @@
+<!-- Homepage -->
+	  
+	 <nav class="navbar navbar-custom navbar-fixed-top text-center">
+					
+						<div class="navbar-header" >
+						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>                        
+						  </button>
+							<img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Los Angeles">
+						
+						</div>
+						
+			<div  class="collapse navbar-collapse" id="myNavbar">		
+				<ul class="nav navbar-nav">
+							<li><a href="<?php echo base_url('Pilipinas/homepage');?>"> Home </a></li>
+							<li><a href="<?php echo base_url('Pilipinas/places/');?>"> Places </a></li>
+							<li class="active" style = "border-radius: 0px 30px 0px 30px; background-color: gray;"><a href="<?php echo base_url('Pilipinas/advertisement/');?>"> Advertisement </a></li>
+							<li><a href="<?php echo base_url('Pilipinas/settings/');?>"> Settings	</a></li>			
+			    </ul> 		  		 
+			</div>
+	</nav>
+	
+<div class = "container-fluid" style = "background-image: url('img/background');">	
+	<div class="row">
+			<br />
+			<br />
+			<br />
+			 
+				  <div class="item">
+					<img src="<?php echo base_url('img/slider_44.jpg') ?>" alt="" style="width:100%;">
+				  </div>
+	</div>			  
+		
+
+</div>
