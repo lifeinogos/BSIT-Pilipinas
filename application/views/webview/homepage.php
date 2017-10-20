09316e83b9c2876c06e0ce367314a2d0036c83e0
Joanalyn <31683116+jlynvz@users.noreply.github.com>
2017-10-20 14:33:56 +0800

    Update homepage.php

--- application/views/webview/homepage.php
+++ application/views/webview/homepage.php
@@ -203,17 +203,6 @@
 
 
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
