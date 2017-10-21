15b5547bcaf0fd092a80b03c067a0410995e724c
Katrina Curbi <31692151+inspiritmelody@users.noreply.github.com>
2017-10-21 10:00:18 +0800

    Add files via upload

--- Kat/about.php
+++ Kat/about.php
@@ -35,54 +35,11 @@
 	</nav>
 	<div class = "container-fluid" style = "background-image: url('img/background')">	
 
-	<div class = "row">
-	<br />
-	<br />
-	  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-	  
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-		  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		  <li data-target="#myCarousel" data-slide-to="1"></li>
-		  <li data-target="#myCarousel" data-slide-to="2"></li> 
-		  <li data-target="#myCarousel" data-slide-to="3"></li>
-		  <li data-target="#myCarousel" data-slide-to="4"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner" style = "box-shadow: 0px 2px 35px black;">
-		  <div class="item active">
-			<img src="<?php echo base_url('img/slider_1.jpg') ?>" alt="welcome to philippines" style="width:100%;">
-		  </div>
-
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_2.jpg') ?>" alt="" style="width:100%;">
-		  </div>
-		
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_3.jpg') ?>" alt="" style="width:100%;">
-		  </div>
-		  
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_4.jpg') ?>" alt="" style="width:100%;">
-		  </div>
-		  
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_5.jpg') ?>" alt="" style="width:100%;">
-		  </div>
-		</div>
-
-		<!-- Left and right controls -->
-		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		  <span class="glyphicon glyphicon-chevron-left"></span>
-		  <span class="sr-only">Previous</span>
-		</a>
-		<a class="right carousel-control" href="#myCarousel" data-slide="next">
-		  <span class="glyphicon glyphicon-chevron-right"></span>
-		  <span class="sr-only">Next</span>
-		</a>
-	  </div>
+<!-- main pic for about -->
+	<div class = "row" style = "margin-top: 20px;">
+		  <div class="item"> <img src="<?php echo base_url('img/slider_3_travel.jpg') ?>" alt="" style="width:100%;">  </div>  
 	</div>
+	
 	<div class = "row text-center" id = "about2">
 	
 		<div class = "col-md-6">
@@ -100,10 +57,12 @@
 				It also aims to advocate local businessmen in promoting Filipino-pride goods and services.</p>
 		</div>
 	</div>
+	
+	<!-- 2nd layer -->
 	<div class = "row">
 		<div class = "col-md-6">
 		<br />
-			<img src="<?php echo base_url('img/featured_1.jpg') ?>" alt="" style="width:60%; padding-bottom:20px; padding-left:5px; ">
+			<img src="<?php echo base_url('img/featured_1.jpg') ?>" alt="" style="width:100%; padding-bottom:20px; padding-left:5px; ">
 		</div>
 
 		<div class = "col-md-6">
@@ -118,20 +77,16 @@
 						<li>Reviews that will help in suggestion for future development</li>
 						<li>Advertisements for local products</li>
 					</ul>
-			
 			</p>
 		</div>
 	</div>
 
-
-	
- <!-- end ng content-->
 	
 	<div class = "row text-center" id ="embed">
 	<br />
 		<div class = "col-md-6">
 			<!-- embed -->
-			<iframe width="100%" height="315" src="https://www.youtube.com/embed/ADNgEHFDYzo" frameborder="0" allowfullscreen></iframe>
+			<iframe width="100%" height="315" src="https://www.youtube.com/embed/ADNgEHFDYzo" frameborder="0" allowfullscreen style="padding-bottom:20px;"></iframe>
 		</div>
 		<div class = "col-md-6">
 			<h1> Discover more with videos</h1>
@@ -148,28 +103,29 @@
 	<div class = "row" id = "footer">
 	
 		<div class = "col-md-6">
-			<h4> DISCOVER MORE </h4>
-			<p style="padding-left: 10px;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
-			Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa</p>
+			<h6>DISCOVER MORE</h6>
+			<div id="p">Pili-Pinas serves as an online tourist hub that showcase detailed information about the country’s heritage, 
+			culture, and natural landmarks. It also provides advertising of local products, reviews and suggestions from users,
+			as well as listed places to visit.</div>
+			
+		</div>
+		
+		<div class = "col-md-2">
+			<h6>GET CONNECTED</h6>
+			<div id="juan"><a href =""><img src = "<?php echo base_url('img/facebook.png') ?>" alt="Facebook">&nbsp;Like us on Facebook</a></div>
+			<div id="juan"><a href =""><img src = "<?php echo base_url('img/twitter.png') ?>" alt="Twitter">&nbsp;Follow us on Twitter</a></div>
 			
 		</div>
 		<div class = "col-md-2">
-			<h4> GET CONNECTED </h4>
-			<div id="juan"><a href =""><img src = "<?php echo base_url('img/facebook.png') ?>" width = "15%" class="img-responsive" alt="Facebook"><p>&nbsp;Like us on Facebook</p></a></div>
-			<div id="juan"><a href =""><img src = "<?php echo base_url('img/twitter.png') ?>" width = "15%" class="img-responsive" alt="Twitter"><p>&nbsp;Follow us on Twitter</p></a></div>
-			
-		</div>
-		<div class = "col-md-2">
-			<h4> CONTACT INFORMATION </h4>
-			<div id="juan"><img src = "<?php echo base_url('img/locate.png') ?>" width = "13%" class="img-responsive" alt="Address">Pili-Pinas</div>
-			<div id="juan">Ayala Boulevard, <br />Ermita Manila</div>
-			<div id="juan"><img src = "<?php echo base_url('img/mobile.png') ?>" width = "13%" class="img-responsive" alt="Phone">(+63) 9152987364</div>
-			<div id="juan"><img src = "<?php echo base_url('img/mail.png') ?>" width = "13%" class="img-responsive" alt="Mail">pili_pinas@gmail.com</div>
+			<h6>CONTACT INFORMATION</h6>
+			<div id="juan"><img src = "<?php echo base_url('img/locate.png') ?>" alt="Address" width = "12%" height="12%">Pili-Pinas <br />Ayala Boulevard, <br />Ermita Manila</div>
+			<div id="juan"><img src = "<?php echo base_url('img/mobile.png') ?>" alt="Phone" width = "12%" height="12%">(+63) 9152987364</div>
+			<div id="juan"><img src = "<?php echo base_url('img/mail.png') ?>" alt="Mail" width = "12%" height="12%">pili_pinas@gmail.com</div>
 		
 		</div>
 		
 		<div class = "col-md-2">
-			<h4> QUICK LINKS </h4>
+			<h6>QUICK LINKS</h6>
 			<div id="qlinks"><a href= "<?php echo base_url('Pilipinas');?>">Home</a></div>
 			<div id="qlinks"><a href= "<?php echo base_url('Pilipinas/explore');?>">Explore</a></div>
 			<div id="qlinks"><a href= "<?php echo base_url('Pilipinas/history');?>">History</a></div>
@@ -178,5 +134,6 @@
 		
 	</div>
 	
-	<div class="">
+	<div class="row text-center" id="ARR">
+		<h6>All Rights Reserved 2017<h6>
 	</div>
