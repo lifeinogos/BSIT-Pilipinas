869ef967a6ba32c0bc833869df800f74e89d989c
Joanalyn <31683116+jlynvz@users.noreply.github.com>
2017-10-21 14:08:22 +0800

    travel-latest w/ button icons
    travel-latest w/ button icons

--- joana/travel.php
+++ joana/travel.php
@@ -1,31 +1,138 @@
-<!-- Travel page -->
 
+
+<!-- Travel page -->
+<style>
+#rig {
+    max-width:1200px;
+    margin:0 auto; /*center aligned*/
+    padding:0;
+    font-size:0; /* Remember to change it back to normal font size if have captions */
+    list-style:none;
+    background-color:white;
+}
+#rig li {
+    display: inline-block;
+    *display:inline;/*for IE6 - IE7*/
+    width:25%;
+    vertical-align:middle;
+    box-sizing:border-box;
+    margin:0;
+    padding:0;
+}
+        
+/* The wrapper for each item */
+.rig-cell {
+    /*margin:12px;
+    box-shadow:0 0 6px rgba(0,0,0,0.3);*/
+    display:block;
+    position: relative;
+    overflow:hidden;
+}
+        
+/* If have the image layer */
+.rig-img {
+    display:block;
+    width: 100%;
+    height: auto;
+    border:none;
+    transform:scale(1);
+    transition:all 1s;
+}
+
+#rig li:hover .rig-img {
+    transform:scale(1.05);
+}
+        
+/* If have the overlay layer */
+.rig-overlay {
+    position: absolute;
+    display:block;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    background: #3DC0F1 url(img/link.png) no-repeat center 20%;
+    background-size:50px 50px;
+    opacity:0;
+    filter:alpha(opacity=0);/*For IE6 - IE8*/
+    transition:all 0.6s;
+}
+#rig li:hover .rig-overlay {
+    opacity:0.8;
+}
+
+/* If have captions */
+.rig-text {
+    display:block;
+    padding:0 30px;
+    box-sizing:border-box;
+    position:absolute;
+    left:0;
+    width:100%;
+    text-align:center;
+    text-transform:capitalize;
+    font-size:18px;
+    font-weight:bold;
+    font-family: 'Oswald', sans-serif;
+    font-weight:normal!important;
+    top:40%;
+    color:white;
+    opacity:0;
+    filter:alpha(opacity=0);/*For older IE*/
+    transform:translateY(-20px);
+    transition:all .3s;
+}
+#rig li:hover .rig-text {
+    transform:translateY(0px);
+    opacity:0.9;
+}
+
+@media (max-width: 9000px) {
+    #rig li {
+        width:19%;
+    }
+}
+
+@media (max-width: 700px) {
+    #rig li {
+        width:19%;
+    }
+}
+
+@media (max-width: 550px) {
+    #rig li {
+        width:19%;
+    }
+
+</style>
 	 <nav class="navbar navbar-custom navbar-fixed-top text-center">
 					
 						<div class="navbar-header" >
 						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-							<span class="icon-bar"></span>
+							<span class="icon-bar" style ="color: red;"></span>
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>                        
 						  </button>
-							<img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Los Angeles">
-						
+						 <a href="<?php echo base_url('Pilipinas');?>"><img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Pilipinas"></a>
 						</div>
-						
+				
+				
+				
 			<div  class="collapse navbar-collapse" id="myNavbar">
 						
 				<ul class="nav navbar-nav">
-							<li><a href="<?php echo base_url('Pilipinas/home/');?>">Home</a></li>
-							<li class="active" ><a href="<?php echo base_url('Pilipinas/travel/');?>">Travel
-							<li><a href="<?php echo base_url('Pilipinas/history/');?>">History
-							<li><a href="<?php echo base_url('Pilipinas/about/');?>">About				
+							<li><a href="<?php echo base_url('Pilipinas');?>">Home</a></li>
+							<li class="active" ><a href="<?php echo base_url('Pilipinas/travel/'); echo '?category=0';?>">Explore</a></li>
+							<li><a href="<?php echo base_url('Pilipinas/history/');?>">History</a></li>
+							<li><a href="<?php echo base_url('Pilipinas/about/');?>">About			</a></li>	
 			    </ul> 
 						  
-				 <ul class = "nav navbar-nav " style = "margin-top: -10px;">
-						  <li> 
-								<form action="" method="get" id="search">	
-									<input type="text" name="search" class="search" value = "Search places"/> 
-									<input type="submit" value="Search" />
+				 <ul class = "nav navbar-nav pull-right " style = "margin-top: -10px;">
+						  <li  id="search-bar"> 
+								<form action="<?php echo base_url('Pilipinas/search/');?>" method="get" id="search">	
+									<input type="text" name="search" class="search" value = "" placeholder = "Search places" style ="color: black"/> 
+									<button type="submit" class="search" ><span class="glyphicon glyphicon-search"></span></button>
 								</form> 
 						  </li>
 				 </ul>		 		 
@@ -33,326 +140,97 @@
 	</nav>
 	
 <div class = "container-fluid" style = "background-image: url('img/background')">	
-
-	<div class = "row">
-	<br />
-	<br />
-	 
-		  <div class="item">
-			<img src="<?php echo base_url('img/slider_5.jpg') ?>" alt="" style="width:100%;">
-		  </div>
-		  
+	<!-- main pic for travel -->
+	<div class = "row" style = "margin-top: 20px;">
+		  <div class="item"> <img src="<?php echo base_url('img/slider_5_travel.jpg') ?>" alt="" style="width:100%;">  </div>  
 	</div>
 
 	<!-- Content -->
-<div class="row">
-<br />
-<div class="col-md-6">
-<br />
-	<div class="row text-center" >
-		<h1 style="color:#C92E2E;"> CHOOSE YOUR REGION HERE</h1>
-		<h4 style="color:#b9b5b5;"> 
-				How do I know where I’m going? 
-			<br />
-				Then each island group is divided into regions and <br /> there are more than seventeen (17) different Philippine regions. 
-			<br />
-				Luzon has eight (8) regions.	<br />
-				Visayas has three (3) regions.	<br />
-				Mindanao has six (6) regions.	<br />
-				</h4>
-				
-			<form action="" method="get" id="search">	
-				<input type="text" name="search" class="search" value = "Enter Region...."/> 
-				<input type="submit" value="Search" />
-			</form> 
-				<br />
-		<select class="selectpicker" multiple title="Region...">
-			  <option>Region 1</option>
-			  <option>Region 2</option>
-			  <option>Region 3</option>
-			  <option>Region 4</option>
-			  <option>Region 5</option>
-			  <option>Region 6</option>
-			  <option>Region 7</option>
-			  <option>Region 8</option>
-			  <option>Region 9</option>
-			  <option>Region 10</option>
-			  <option>Region 11</option>
-			  <option>Region 12</option>
-			  <option>Region 13</option>
-			  <option>Region 14</option>
-			  <option>Region 15</option>
-			  <option>Region 16</option>
-			  <option>Region 17</option>	  
-		</select>
-		
-	<!--		<input type="button" class="btn btn-danger" id="bt" value="Regions" onclick="showMin()" />
-		
-							<li class="dropdown">
-									
-									<ul class="dropdown-menu">
-									  <li><a href="<?php echo base_url('Pilipinas/travel/');?>"> Breathtaking Tourist Spots </a></li>
-									  <li class="divider"></li>
-									  <li><a href="<?php echo base_url('Pilipinas/travel2/');?>">Must Try Outdoor Activities</a></li>
-									  <li class="divider"></li>
-									  <li><a href="<?php echo base_url('Pilipinas/travel3/');?>">Calendar of Festivals</a></li> 
-									</ul>
-								  </li>
-			
-			
-			<div style="color:red" class="dropdown">
-				<button style="color: #ffffff;" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">REGIONS
-			   <span style="color: #ffffff;" class="caret"></span></button>
-				   <ul class="dropdown-menu">
-					<li style="color:mediumblue;"> <a href="#"> Region 1 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 2 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 3 </a></li>	
-					<li style="color:mediumblue;"> <a href="#"> Region 4 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 5 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 6 </a></li>	
-					<li style="color:mediumblue;"> <a href="#"> Region 7 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 8 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 9 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 10 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 11 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 12 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 13 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 14 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 15 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 16 </a></li>
-					<li style="color:mediumblue;"> <a href="#"> Region 17 </a></li>
-				   </ul>
-			</div> 
-						
-			-->
-			
-	</div>	
-	 
-</div>
- 
 	
-        <div class="col-md-6">	
-				<ul id="rig">
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti1.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Lorem Ipsum Dolor</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti2.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Consectetur Adipiscing</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti3.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Vivamus ut Felis</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti4.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Curabitur tempus</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti5.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Sed et nunc luctus</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti6.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Suspendisse tellus</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti1.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">pellentesque odio</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti2.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Nam euismod</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti3.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Vestibulum ultricies</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti4.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">quis interdum sapien</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti5.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Maecenas vitae</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/desti6.png') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Proin Scelerisque</span>
-						</a>
-					</li>
-				</ul>
-			</div>
-		
-		
-		
-		
-</div> 	<!-- End of Content-->
-
-	<!-- Top Destinations  -->
-	<div class = "row" id = "topdesti" >
-		<br />
-		<br />
-		<div class ="col-md-6">
-		<a href = ""> <!--<img src="<?php echo base_url('img/products.jpg') ?>" alt="" style="width:90%;"> </a> -->
+	
+	
+	
+	<div class = "row text-center" style = "">
+		<div class = "col-md-1"></div>
+		<div class = "col-md-10">
+			<h1>Pick one to Discover </h1>
 		</div>
-		<div class ="col-md-6">
-		</div>
+		<div class = "col-md-1"></div>
 	</div>
-	<div class = "row" id = "topdesti">
-		<br />
-		<div class = "col-md-4">
-			<!--<a href = ""> <img src="<?php echo base_url('img/featured_desti.png') ?>" alt="" style=" width:90%; height: 80%; "> </a>-->
+	
+	<div  class = "row text-center" >
+		<div class = "col-xs-1"></div>
+		<div id = "travel" class = "col-sm-2 active" >
+			<a href =" <?php echo base_url('Pilipinas/travel/'); echo '?category=0'; ?>"> <img src="<?php echo base_url('img/all.png') ?>" alt="" style=" width: 50px; height: 50px;" > </a>
 		</div>
-		<div class = "col-md-4">
-		<h1 style="color:#C92E2E;"> REGION 1</h1>
-		<!--	<a href = ""> <img src="<?php echo base_url('img/festibg.jpg') ?>" alt="" style=" width:90%; height: 80%; "> </a>-->
+		
+		
+		
+		
+		<div  id = "travel" class = "col-xs-2">
+		<a href =" <?php echo base_url('Pilipinas/travel/'); echo '?category=1'; ?>"> <img src="<?php echo base_url('img/city.png') ?>" alt="" style=" width: 50px; height: 50px;"> </a>
 		</div>
-		<div class = "col-md-4">
-		<!--	<a href = ""> <img src="<?php echo base_url('img/products.jpg') ?>" alt="" style=" width:90%; height: 80%; "> </a>-->
+		<div id = "travel" class = "col-xs-2">
+		<a href =" <?php echo base_url('Pilipinas/travel/'); echo '?category=2'; ?>"> <img src="<?php echo base_url('img/beach.png') ?>" alt="" style=" width: 50px; height: 50px;"> </a>
 		</div>
-	</div> <!-- end of top desti -->
+		<div id = "travel"  class = "col-xs-2">
+		<a href =" <?php echo base_url('Pilipinas/travel/'); echo '?category=3'; ?>"> <img src="<?php echo base_url('img/mountain.png') ?>" alt="" style=" width: 60px; height: 60px;"> </a>
+		</div>
+		<div id = "travel" class = "col-xs-2">
+		<a href =" <?php echo base_url('Pilipinas/travel/'); echo '?category=4'; ?>"> <img src="<?php echo base_url('img/heritage.png') ?>" alt="" style=" width: 50px; height: 50px;" > </a>
+		</div>	
+		<div class = "col-xs-1"></div>
+    </div>
 	
 	
-<ul id="rig">
-<h1 style="color:#FFFFFF;"> Top Destinations </h1>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/d1.jpg') ?> " >
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Paoay Church</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/d2.jpg') ?> " >
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Bangi Windmills</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/d3.jpg') ?> " >
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Kapurpurawan Rock Formation</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/d4.jpg') ?> " >
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Calle Crisollogo</span>
-						</a>
-					</li>
-<h1 style="color:#FFFFFF;"> Activities </h1>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/a4.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Paoay Sand Dune</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/a3.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Suspendisse tellus</span>
-						</a>
-					</li>				
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/a2.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">pellentesque odio</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/a1.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Nam euismod</span>
-						</a>
-					</li>
-<h1 style="color:#FFFFFF;"> Products </h1>	
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/f1.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Bagnet</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/f2.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Chichacorn</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/f3.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Empanada</span>
-						</a>
-					</li>
-					<li>
-						<a class="rig-cell" href="#">
-							<img class="rig-img" src="<?php echo base_url('img/f4.jpg') ?> ">
-							<span class="rig-overlay"></span>
-							<span class="rig-text">Ilocos Basi</span>
-						</a>
-					</li>
+<div class = "row text-center" id = "travelcontent" >
+			
+			<h2> List of All <?php echo $label;?> Places</h2>
+			<!--
+			<div class="se-pre-con text-left"> <img src = "<?php echo base_url('img/preloader_10.gif') ;?> "> </div>
+			-->
+			<ul id="rig" style = "box-shadow: 4px 26px 21px -18px gray;" >
+					<?php 
+					foreach ($places as $n){
+						echo '
+						<li>
+							<a class="rig-cell" href="'; echo base_url('Pilipinas/details'); echo '?place_id='.$n['place_id']; echo' ">
+								<div class="thumbnail">
+									<img class="rig-img" src="'; echo base_url('img/1place/1200x800/'.$n['image']);echo' ">
+									<div class="caption post-content">
+										<h3>'; echo $n['name']; echo '</h3>
+									</div>
+								</div>
+								
+								
+								<span class="rig-overlay"></span>
+								<span class="rig-text">'; echo $n['name']; echo ' </span>
+							</a>
+						</li>
+						';
+					}
+					?>
+					
+					
+				
 				</ul>
+	</div>
 	
-	
+ 
 
-	
-	
+
+
 	<!-- footer -->
-	<div class = "row text-center" id = "footer">
-		<div class = "col-md-4">
-			 <img src = "<?php echo base_url('img/logo.png') ?>" width = "100%" class="img-responsive" alt="Los Angeles">
+	<div class = "row text-center" id = "foooter" style="background-color:#bcbcbc; color: black;">
+		<div class = "col-md-4" align="center">
+			 <img src = "<?php echo base_url('img/logo.png') ?>" width = "60%" class="img-responsive" alt="Los Angeles" align="center">
 		</div>
+		<div class = "col-md-2">
+		</div>
+
 		<div class = "col-md-2 text-center">
 			<h3> FOLLOW US ON</h3>
-			<a href = ""> Twitter</a>
-			<a href = ""> Facebook  </a>
+		<center> <img src = "<?php echo base_url('img/fbicon1.jpg') ?>" width = "20%" class="img-responsive" alt="Los Angeles"></center>
 			
 		</div>
 		<div class = "col-md-2">
@@ -362,18 +240,16 @@
 			<h4> TUP Manila</h4>
 		</div>
 		<div class = "col-md-2">
-			<h3> ABOUT US</h3>
-			<h4> Sitemap</h4>
-			<h4> Privacy Policy</h4>
-			<h4> Terms and Conditions</h4>
+			<h3> QUICK LINKS</h3>
+			<h4> <a href=""> Travel</a><br /></h4>
+			<h4><a href=""> History</a><br /><h4>
+			<h4><a href=""> About</a><br /></h4>
 		</div>
-		<div class = "col-md-2">
-		</div>
-	</div>
+		</div>	
 	
-	<div class = "row text-center" id ="ARR">
+	<div class = "row text-center" class ="ARR" style="background-color:#000000;" >
 		<h3> All Rights Reserved 2017<h3>
 	</div>
 
-
 </div>
+
