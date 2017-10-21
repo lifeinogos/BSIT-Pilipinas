5b9b3827d1e176a10385a6d443158b038e0c8a83
Ogos <31694161+lifeinogos@users.noreply.github.com>
2017-10-22 02:12:46 +0800

    updated contoller for new review logic

--- mercado/Pilipinas.php
+++ mercado/Pilipinas.php
@@ -139,6 +139,7 @@ class Pilipinas extends CI_Controller {
 			if($review_list != 'false'){
 				foreach ($review_list as $n){
 					$list = array(
+						'review_id' => $n['review_id'],
 						'rating' => $n['rating'],
 						'reviewer' => $n['reviewer'],
 						'review' => $n['review']
@@ -191,27 +192,32 @@ class Pilipinas extends CI_Controller {
 				
 			$data['title'] = "Pili-pinas";
 			$data['contents'] = "Display bootstrap contents";
-			$rate = $_GET['rating'];
-			$reviewer = $_GET['reviewname'];
-			$review = $_GET['reviewmismo'];
+			$rate = $_POST['rating'];
+			$reviewer = $_POST['reviewname'];
+			$review = $_POST['reviewmismo'];
 			
-			$placeid = $_GET['place_id'];	
+			$placeid = $_POST['place_id'];	
 			
 			
-			$new_reviewid = $this->pinasmodel-> reviewid();
+			$new_reviewid = $this->pinasmodel->latest_pendingreviewid();
 			
 			$new_reviewid = $new_reviewid+1;
 			
 			$new_review = array (
-				'rating' => $rate,
+				
+				
+				
+				
+				'treview_id' => $new_reviewid,
+				'place_id' => $placeid,
 				'reviewer' => $reviewer,
 				'review' => $review,
-				'place_id' => $placeid,
-				'review_id' => $new_reviewid
+				'rating' => $rate
+				
 			); //i array ang ipapasa
 			
 			
-			$reviewtablename = "review";      // name of table
+			$reviewtablename = "pending_review";      // name of table
 			$new_review = $this->pinasmodel->review_insert($reviewtablename, $new_review); // send to model to insrt
 			
 			
@@ -225,7 +231,8 @@ class Pilipinas extends CI_Controller {
 					$list = array(
 						'rating' => $n['rating'],
 						'reviewer' => $n['reviewer'],
-						'review' => $n['review']
+						'review' => $n['review'],
+						'reports' => $n['reports']
 					);
 					$details_review[] = $list;
 				
@@ -240,25 +247,25 @@ class Pilipinas extends CI_Controller {
 			
 			
 			
-			//rating
-			$new_rating = $this->pinasmodel->rating($rate, $placeid);
-			$details_complete = null;
+			// rating
+			 $new_rating = $this->pinasmodel->place_page($placeid);
+			 $details_complete = null;
 		
 			//KUNIN ULIT LAMAN NUNG PAGE
 			$id = array(
-				'place_id' => $new_rating['place_id'],
-				'name' => $new_rating['name'],
-				'description' => $new_rating['description'],
-				'locality' => $new_rating['locality'],
-				'province' => $new_rating['province'],
-				'region' => $new_rating['region'],
-				'rating' => $new_rating['rating'],
-				'vote' =>  $new_rating['vote'],
-				'image' => $new_rating['image'],		
-				'thumbs' => $new_rating['thumbs'],		
-				'image2' => $new_rating['image2'],		
-				'image3' => $new_rating['image3'],		
-				'map' => $new_rating['map'],		
+				'place_id' => $new_rating[0]['place_id'],
+				'name' => $new_rating[0]['name'],
+				'description' => $new_rating[0]['description'],
+				'locality' => $new_rating[0]['locality'],
+				'province' => $new_rating[0]['province'],
+				'region' => $new_rating[0]['region'],
+				'rating' => $new_rating[0]['rating'],
+				'vote' =>  $new_rating[0]['vote'],
+				'image' => $new_rating[0]['image'],		
+				'thumbs' => $new_rating[0]['thumbs'],		
+				'image2' => $new_rating[0]['image2'],		
+				'image3' => $new_rating[0]['image3'],		
+				'map' => $new_rating[0]['map'],		
 			);
 			
 			
@@ -411,7 +418,7 @@ class Pilipinas extends CI_Controller {
 					
 		}
 		public function logout(){
-			
+		
 		}
 		
 		//the admin
@@ -468,6 +475,7 @@ class Pilipinas extends CI_Controller {
 						
 			$file_info = $this->upload->data('');
 			$upload_image3 = $file_info['file_name'];
+			
 			$place_id = $this ->pinasmodel -> latestid();
 			
 			//get the latest values
@@ -528,18 +536,12 @@ class Pilipinas extends CI_Controller {
 		public function editplace(){
 			$placeid = $_GET['place_id'];
 			$data['title'] = "Pili-pinas";
-			$details_page= $this->pinasmodel->place_page($placeid);
 			
+			$details_page= $this->pinasmodel->place_page($placeid);	
 			$review_list = $this->pinasmodel->review_getlist($placeid);
-			
-			
-			
-			
-			
+				
 			// print_r ($place_id);
 			$details_complete = null;
-		
-			
 				$id = array(
 					'place_id' => $details_page[0]['place_id'],
 					'name' => $details_page[0]['name'],
@@ -690,7 +692,6 @@ class Pilipinas extends CI_Controller {
 			$this->load->view('webview/admin_places', $data);
 		}
 		
-		
 		//adminlogin
 		public function admin_login(){
 			$data['title'] = "Pili-pinas";
@@ -728,4 +729,515 @@ class Pilipinas extends CI_Controller {
 			$this->load->view('include/header', $data);
 			$this->load->view('webview/admin_login', $data);
 		}
+
+		
+		
+		
+		
+		//-----------------------------REVIEWS --------------------------
+		//manage pending reviews
+		public function reviews(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+			
+			$reviews = $this->pinasmodel->pendingreviews();
+			
+			//pag may laman
+			if($reviews != 'false')	{
+				foreach($reviews as $n){
+					$id = array(
+						'rating'   		 => $n['rating'],
+						'reviewer'        => $n['reviewer'],
+						'review'		 => $n['review'],
+						'place_id'   	 => $n['place_id'],
+						'treview_id'		  => $n['treview_id']
+						
+					);
+					$review[]=$id;
+			
+				}
+			
+			
+				$data['review'] = $review; 
+				
+				$count = $this->pinasmodel->count_pending();
+				$data['count'] = $count;	
+			}
+			//pag walang laman
+			else {
+				$data['review'] = 'No pending reviews';
+				$data['count'] = '0';
+				
+			}
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_reviews', $data);
+			
+		}
+		
+		//delete pending review 
+		public function delete_pending(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+			
+			$treview_id = $_POST['treview_id'];
+			$table = "pending_review";
+			$this->pinasmodel->delete_pending($table, $treview_id);
+			
+			//kunin ulit bagong list ng pending
+			$reviews = $this->pinasmodel->pendingreviews();
+			if($reviews != 'false')	{
+				foreach($reviews as $n){
+					$id = array(
+					'rating'   		 => $n['rating'],
+					'reviewer'        => $n['reviewer'],
+					'review'		 => $n['review'],
+					'place_id'   	 => $n['place_id'],
+					'treview_id'		  => $n['treview_id']
+					
+					);
+					$review[]=$id;
+			
+				}
+			
+			
+				$data['review'] = $review; 
+				
+				$count = $this->pinasmodel->count_pending();
+				$data['count'] = $count;	
+			}
+			//pag walang laman
+			else {
+				$data['review'] = 'No pending reviews';
+				$data['count'] = '0';
+			}
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_reviews', $data);
+		}
+		
+		
+		//add pending review to approved
+		public function add_pending(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+			$treview_id = $_POST['treview_id'];
+			$table_treview = 'pending_review';
+			
+			//kunin ang laman ni pending revew row
+			$treview_row = $this->pinasmodel->getpendingreviewrow($table_treview, $treview_id);
+			$row = array(
+					'rating'   	  => $treview_row[0]['rating'],
+					'reviewer'    => $treview_row[0]['reviewer'],
+					'review'	  => $treview_row[0]['review'],
+					'place_id'    => $treview_row[0]['place_id'],
+					'treview_id'	  => $treview_row[0]['treview_id'],
+				);
+				$place_id = $row['place_id'];
+				$rating = $row['rating'];
+				
+				$details_page= $this->pinasmodel->place_page($place_id);	
+				$table = 'review';
+				
+				
+				$id = array(
+					'place_id' => $details_page[0]['place_id'],
+					'name' => $details_page[0]['name'],
+					'description' => $details_page[0]['description'],
+					'category' => $details_page[0]['category'],
+					'locality' => $details_page[0]['locality'],
+					'province' => $details_page[0]['province'],
+					'region' => $details_page[0]['region'],
+					'image' => $details_page[0]['image'],
+					'image2' => $details_page[0]['image2'],
+					'image3' => $details_page[0]['image3'],
+					'thumbs' => $details_page[0]['thumbs'],
+					'rating_total' => $details_page[0]['rating_total'],
+					'rating' => $details_page[0]['rating'],
+					'map' => $details_page[0]['map'],
+					'vote' => $details_page[0]['vote']	
+				);
+				
+				$newrating = 0;
+				$newvote = 0;
+				$newratingtotal = 0;
+				
+				$newratingtotal = $id['rating_total'] + $rating;
+				$newvote = $id['vote'] + 1;
+				$newrating = $newratingtotal / $newvote;
+				
+				
+				$pass = array(
+				'place_id' => $id['place_id'],
+				'name' => $id['name'],
+				'description' => $id['description'],
+				'category' => $id['category'],
+				'locality' => $id['locality'],
+				'province' => $id['province'],
+				'region' => $id['region'],
+				
+				'thumbs' => $id['thumbs'],
+				'image' => $id['image'],
+				'image2' => $id['image2'],
+				'image3' => $id['image3'],
+				'rating_total' =>  $newratingtotal,
+				'rating' => $newrating,
+				'vote' => $newvote,
+				'map' => $id['map']
+			);
+			$table2 = 'place';
+			//update place 
+			$this ->pinasmodel -> edit_place($table2,$pass,$place_id);
+			
+			//update review.tbl
+			//hanapin sa review.tbl ang latest review id
+			$review_id = 0;
+			$review_id = $this ->pinasmodel -> latest_reviewid();
+			$haha = array(
+				'id' => $review_id['review_id']
+			);
+			
+			$new_reviewid = $haha['id']+ 1;
+			
+			$new_review = array (
+				'rating' => $treview_row[0]['rating'],
+				'reviewer' => $treview_row[0]['reviewer'],
+				'review' => $treview_row[0]['review'],
+				'review_id' => $new_reviewid,
+				'place_id' => $place_id,
+				'reports' => 0
+			); //i array ang ipapasa
+			
+			
+			$reviewtablename = "review"; 
+			$this->pinasmodel->moveto_approved($reviewtablename, $new_review);
+				
+				
+			//i-delete na sa pending_review
+			
+			$this->pinasmodel->delete_pending($table, $treview_id);
+			
+			//kunin ulit bagong list ng pending
+			$reviews = $this->pinasmodel->pendingreviews();
+			if($reviews != 'false')	{
+				foreach($reviews as $n){
+					$id = array(
+					'rating'   		 => $n['rating'],
+					'reviewer'        => $n['reviewer'],
+					'review'		 => $n['review'],
+					'place_id'   	 => $n['place_id'],
+					'treview_id'		  => $n['treview_id']
+					
+					);
+					$review[]=$id;
+			
+				}
+			
+			
+				$data['review'] = $review; 
+				
+				$count = $this->pinasmodel->count_pending();
+				$data['count'] = $count;	
+			}
+			//pag walang laman
+			else {
+				$data['review'] = 'No pending reviews';
+				$data['count'] = '0';
+			}
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_reviews', $data);
+			
+			
+			
+		}
+		
+		//manage approved reviews
+		public function reviews_2(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+			
+			$reviews = $this->pinasmodel->approvedreviews();	
+			
+			if($reviews != 'false')	{
+				foreach($reviews as $n){
+					$id = array(
+					'rating'   		 => $n['rating'],
+					'reviewer'        => $n['reviewer'],
+					'review'		 => $n['review'],
+					'place_id'   	 => $n['place_id'],
+					'review_id'		  => $n['review_id'],
+					
+					'reports'	  => $n['reports']
+					);
+					$review[]=$id;
+			
+				}
+			
+			
+				$data['review'] = $review; 
+				
+				$count = $this->pinasmodel->count_pending();
+				$data['count'] = $count;	
+			}
+			//pag walang laman
+			else {
+				$data['review'] = 'No pending reviews';
+				$data['count'] = '0';
+			}
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_reviews_2', $data);
+			
+		}
+		
+		//delete an approved review
+		public function delete_approved(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+				$vm = $this;
+				$review_id = $_GET['review_id'];
+				$table = "review";
+				$place_id = $_GET['place_id'];
+				$rating = $_GET['rating'];
+			
+			//delete review from database
+			$this->pinasmodel->delete_approved($table, $review_id);
+			
+			
+			
+			//kunin ang updated na list ng reviews
+			$reviews = $this->pinasmodel->approvedreviews();
+			//bilangin ulit kung ilang reviews
+			$count = $this->pinasmodel->count_approved();
+			foreach($reviews as $n){
+				$id = array(
+					'rating'   		 => $n['rating'],
+					'reviewer'        => $n['reviewer'],
+					'review'		 => $n['review'],
+					'place_id'   	 => $n['place_id'],
+					'review_id'		  => $n['review_id'],
+					'likes'	  		=> $n['likes'],
+					'reports'	  => $n['reports']
+				);
+				$review[]=$id;
+			
+			}
+			$data['review'] = $review; 
+			$data['count'] = $count;	
+			
+			//update the place record to delete number of reviews and minus it to the rating
+			$details_page= $this->pinasmodel->place_page($place_id);	
+			
+				$id = array(
+					'place_id' => $details_page[0]['place_id'],
+					'name' => $details_page[0]['name'],
+					'description' => $details_page[0]['description'],
+					'category' => $details_page[0]['category'],
+					'locality' => $details_page[0]['locality'],
+					'province' => $details_page[0]['province'],
+					'region' => $details_page[0]['region'],
+					'image' => $details_page[0]['image'],
+					'image2' => $details_page[0]['image2'],
+					'image3' => $details_page[0]['image3'],
+					'thumbs' => $details_page[0]['thumbs'],
+					'rating_total' => $details_page[0]['rating_total'],
+					'rating' => $details_page[0]['rating'],
+					'map' => $details_page[0]['map'],
+					'vote' => $details_page[0]['vote']	
+				);
+				
+				$newrating = 0;
+				$newvote = 0;
+				$newratingtotal = 0;
+				
+				$newratingtotal = $id['rating_total'] - $rating;
+				$newvote = $id['vote'] - 1;
+				$newrating = $newratingtotal / $newvote;
+				
+			$pass = array(
+				'place_id' => $id['place_id'],
+				'name' => $id['name'],
+				'description' => $id['description'],
+				'category' => $id['category'],
+				'locality' => $id['locality'],
+				'province' => $id['province'],
+				'region' => $id['region'],
+				
+				'thumbs' => $id['thumbs'],
+				'image' => $id['image'],
+				'image2' => $id['image2'],
+				'image3' => $id['image3'],
+				'rating_total' =>  $newratingtotal,
+				'rating' => $newrating,
+				'vote' => $newvote,
+				'map' => $id['map']
+			);
+			$table2 = 'place';
+			$this ->pinasmodel -> edit_place($table2,$pass,$place_id);
+	
+			
+			
+		}
+		// delete via form post
+		public function delete_approved2(){
+			$review_id = $_POST['review_id'];
+			$table_review = 'review';
+			
+			
+			$review_row = $this->pinasmodel->getreviewrow($table_review, $review_id);
+				
+				$row = array(
+					'rating'   	  => $review_row[0]['rating'],
+					'reviewer'    => $review_row[0]['reviewer'],
+					'review'	  => $review_row[0]['review'],
+					'place_id'    => $review_row[0]['place_id'],
+					'review_id'	  => $review_row[0]['review_id'],
+					'reports'	  	  => $review_row[0]['reports']
+					
+				);
+				
+				$place_id = $row['place_id'];
+				$rating = $row['rating'];
+				
+				$details_page= $this->pinasmodel->place_page($place_id);	
+				$table = 'review';
+				
+				
+				$id = array(
+					'place_id' => $details_page[0]['place_id'],
+					'name' => $details_page[0]['name'],
+					'description' => $details_page[0]['description'],
+					'category' => $details_page[0]['category'],
+					'locality' => $details_page[0]['locality'],
+					'province' => $details_page[0]['province'],
+					'region' => $details_page[0]['region'],
+					'image' => $details_page[0]['image'],
+					'image2' => $details_page[0]['image2'],
+					'image3' => $details_page[0]['image3'],
+					'thumbs' => $details_page[0]['thumbs'],
+					'rating_total' => $details_page[0]['rating_total'],
+					'rating' => $details_page[0]['rating'],
+					'map' => $details_page[0]['map'],
+					'vote' => $details_page[0]['vote']	
+				);
+				
+				$newrating = 0;
+				$newvote = 0;
+				$newratingtotal = 0;
+				
+				$newratingtotal = $id['rating_total'] - $rating;
+				$newvote = $id['vote'] - 1;
+				$newrating = $newratingtotal / $newvote;
+				
+				
+				$pass = array(
+				'place_id' => $id['place_id'],
+				'name' => $id['name'],
+				'description' => $id['description'],
+				'category' => $id['category'],
+				'locality' => $id['locality'],
+				'province' => $id['province'],
+				'region' => $id['region'],
+				
+				'thumbs' => $id['thumbs'],
+				'image' => $id['image'],
+				'image2' => $id['image2'],
+				'image3' => $id['image3'],
+				'rating_total' =>  $newratingtotal,
+				'rating' => $newrating,
+				'vote' => $newvote,
+				'map' => $id['map']
+			);
+			$table2 = 'place';
+			$this ->pinasmodel -> edit_place($table2,$pass,$place_id);
+			$this->pinasmodel->delete_approved($table, $review_id);
+			
+			
+			//bilangin ulit kung ilang reviews
+			$count = $this->pinasmodel->count_approved();
+			
+			//kunin ang updated na list ng reviews
+			$reviews = $this->pinasmodel->approvedreviews();
+			foreach($reviews as $n){
+				$id = array(
+					'rating'   		 => $n['rating'],
+					'reviewer'        => $n['reviewer'],
+					'review'		 => $n['review'],
+					'place_id'   	 => $n['place_id'],
+					'review_id'		  => $n['review_id'],
+					'reports'	  => $n['reports']
+				);
+				$review[]=$id;
+			
+			}
+			$data['review'] = $review; 
+			$data['count'] = $count;	
+			
+			
+			
+			
+			
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_reviews_2', $data);
+					
+		}
+		
+		
+		
+		
+		//---------------------------ADMIN ADD ---------------------------------
+		
+		//admin add
+		public function admin_add(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_add', $data);
+		}
+		
+		//addadmin
+		public function adminadd(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+			
+			$name = $_POST['aname'];
+			$user = $_POST['user'];
+			$pass = $_POST['pass'];
+			$pass2 = $_POST['pass2'];
+			
+			if ($name==NULL||$user==NULL||$pass==NULL||$pass2==NULL) {
+				echo 'Please complete all fields';
+			} else if ($pass!==$pass2){
+				echo 'Password does not match the confirm password';
+			} else {
+			$id = $this ->pinasmodel -> latestadminid();
+			
+			$pass = array(
+				'admin_id' => $id + 1,
+				'Name' => $_POST['aname'],
+				'Username' => $_POST['user'],
+				'Password' => $_POST['pass'],
+			);
+			$table = 'admin_accounts';
+			$new=$this ->pinasmodel -> newacc($table,$pass);	
+					
+			}
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_add', $data);
+		}
+		
+		//admin_settings
+		public function settings(){
+			$data['title'] = "Pili-pinas";
+			$data['contents'] = "Display bootstrap contents";
+			
+			$this->load->view('include/header', $data);
+			$this->load->view('webview/admin_settings', $data);
+		
+		}
+		
 }
