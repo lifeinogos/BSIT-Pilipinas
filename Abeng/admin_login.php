07dd0cfd3393d372b8a5c9e525ac270501c5eb93
Abiii <31880805+roserenity@users.noreply.github.com>
2017-10-21 11:06:00 +0800

    underconstruction: admin_login

---
+++ Abeng/admin_login.php
@@ -0,0 +1,16 @@
+<!-- Homepage -->
+<div class = "container-fluid" style = "background-image: url('img/background');">	
+
+<form action="<?php echo base_url('Pilipinas/login/');?>" method="get" id="search">	
+Username: <input name="user"> </input >
+</br>
+Password: <input name="pass"> </input >
+<button type="submit" "> LOGIN</button>
+
+</form>
+
+
+
+
+	
+</div> <!-- end of container-fluid -->
