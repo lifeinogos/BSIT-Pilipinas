2cc31270777ac597c7b8810c18f9f718a70980ed
Robilyn ^-^ <31682985+RAbenoja@users.noreply.github.com>
2017-10-22 12:57:49 +0800

    about

---
+++ robilyn-/about.php
@@ -0,0 +1,164 @@
+<!-- Homepage -->
+	  
+	 
+		 <nav class="navbar navbar-custom navbar-fixed-top text-center">
+					
+						<div class="navbar-header" >
+						  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar" style ="color: red;"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>                        
+						  </button>
+						 <a href="<?php echo base_url('Pilipinas');?>"><img src = "<?php echo base_url('img/logo.png') ?>" width = "100" class="img-responsive" alt="Pilipinas"></a>
+						</div>
+				
+				
+				
+			<div  class="collapse navbar-collapse" id="myNavbar">
+						
+				<ul class="nav navbar-nav">
+							<li><a href="<?php echo base_url('Pilipinas');?>">Home</a></li>
+							<li><a href="<?php echo base_url('Pilipinas/travel/'); echo '?category=0'; ?>">Explore</a></li>
+							<li><a href="<?php echo base_url('Pilipinas/history/');?>">History</a></li>
+							<li class="active" ><a href="<?php echo base_url('Pilipinas/about/');?>">About</a></li>	
+			    </ul> 
+						  
+				 <ul class = "nav navbar-nav pull-right " style = "margin-top: -10px;">
+						  <li  id="search-bar"> 
+								<form action="<?php echo base_url('Pilipinas/search/');?>" method="get" id="search">	
+									<input type="text" name="search" class="search" value = "" placeholder = "Search places" style ="color: black"/> 
+									<button type="submit" class="search" ><span class="glyphicon glyphicon-search"></span></button>
+								</form> 
+						  </li>
+				 </ul>		 		 
+			</div>
+	</nav>
+	<div class = "container-fluid" style = "background-image: url('img/background')">	
+
+	<div class = "row">
+	<br />
+	<br />
+	  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+	  
+
+		<div class="row">
+			<div class="pimg1">
+			<div class="ptext">
+				<span class="border">
+						PARALLAX WEBSITE
+					</span>
+			</div>
+			</div>	
+		
+			<section class="section section-light">
+			<h2> Section One</h2>
+			<p>
+				Lorem ipsum uns dedf df df df jsa j ajd ajd jadjsc dfjsc sdfjkdf sddjfknsd sdjfnsdk dffjsd sdjfsdf sdjfj sdjf kksdk bang bnag bnag bang bang bangtan bangtan
+				bang bnag bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bn=
+				bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan
+				hd sjdds ds d sd sd sd s d sd sd sd sdsd sd erg hjbyjbyvjrb yyj tjdrhv hvh thtrhb vt tvh tvtht  tvhty y vh tvh tyhh th tvh hv  ht 
+			</p>
+			</section>
+		
+			<div class="pimg2">
+			<div class="ptext">
+				<span class="border">
+						IMAGE TWO TEXT
+					</span>
+			</div>
+			</div>	
+			<section class="section section-dark">
+			<h2> Section Two</h2>
+			<p>
+				Lorem ipsum uns dedf df df df jsa j ajd ajd jadjsc dfjsc sdfjkdf sddjfknsd sdjfnsdk dffjsd sdjfsdf sdjfj sdjf kksdk bang bnag bnag bang bang bangtan bangtan
+				bang bang bang bang bang bang bang bang bang bang bang bang bang bang bang 
+				bang bnag bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bn=
+				bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan
+				hd sjdds ds d sd sd sd s d sd sd sd sdsd sd erg hjbyjbyvjrb yyj tjdrhv hvh thtrhb vt tvh tvtht  tvhty y vh tvh tyhh th tvh hv  ht 
+			</p>
+			</section>
+		
+			<div class="pimg3">
+			<div class="ptext">
+				<span class="border">
+						IMAGE THREE TEXT
+					</span>
+			</div>
+			</div>	
+			<section class="section section-dark">
+			<h2> Section Three</h2>
+			<p>
+				Lorem ipsum uns dedf df df df jsa j ajd ajd jadjsc dfjsc sdfjkdf sddjfknsd sdjfnsdk dffjsd sdjfsdf sdjfj sdjf kksdk bang bnag bnag bang bang bangtan bangtan
+				bang bnag bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bn=
+				bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan
+				hd sjdds ds d sd sd sd s d sd sd sd sdsd sd erg hj
+				Lorem ipsum uns dedf df df df jsa j ajd ajd jadjsc dfjsc sdfjkdf sddjfknsd sdjfnsdk dffjsd sdjfsdf sdjfj sdjf kksdk bang bnag bnag bang bang bangtan bangtan
+				bang bnag bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bn=
+				bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan bangtan
+				hd sjdds ds d sd sd sd s d sd sd sd sdsd sd erg hj
+				byjbyvjrb yyj tjdrhv hvh thtrhb vt tvh tvtht  tvhty y vh tvh tyhh th tvh hv  ht 
+			</p>
+			</section>
+
+			<div class="pimg1">
+			<div class="ptext">
+				<span class="border">
+						PARALLAX WEBSITE
+					</span>
+			</div>
+			</div>	
+		</div>
+	
+ <!-- end ng content-->
+	
+	<div class = "row text-center" id ="embed">
+	<br />
+		<div class = "col-md-6">
+			<!-- embed -->
+			<iframe width="100%" height="315" src="https://www.youtube.com/embed/ADNgEHFDYzo" frameborder="0" allowfullscreen></iframe>
+		</div>
+		<div class = "col-md-6">
+			<h1> Discover more with videos</h1>
+			<h3> It's 7,107 times more fun in the Philippines. Watch the official Philippine Department of Tourism AVP  to find out why.</h3>
+			<a href = "https://www.youtube.com/user/morefunph" > Tourism Philippines</a>
+		</div>
+	</div>
+	
+	
+	
+	
+	
+	<!-- footer -->
+	<div class = "row text-center" id = "footer">
+		<div class = "col-md-4">
+			 <img src = "<?php echo base_url('img/logo.png') ?>" width = "100%" class="img-responsive" alt="Los Angeles">
+		</div>
+		<div class = "col-md-2 text-center">
+			<h3> FOLLOW US ON</h3>
+			<a href = ""> Twitter</a>
+			<a href = ""> Facebook  </a>
+			
+		</div>
+		<div class = "col-md-2">
+			<h3> CONTACT US </h3>
+			<h4> 09152987364</h4>
+			<h4> pili_pinas@gmail.com</h4>
+			<h4> TUP Manila</h4>
+		</div>
+		<div class = "col-md-2">
+			<h3> ABOUT US</h3>
+			<h4> Sitemap</h4>
+			<h4> Privacy Policy</h4>
+			<h4> Terms and Conditions</h4>
+		</div>
+		<div class = "col-md-2">
+		</div>
+	</div>
+	
+	<div class = "row text-center" id ="ARR">
+		<h3> All Rights Reserved 2017<h3>
+	</div>
+
+	
+
+</div>
